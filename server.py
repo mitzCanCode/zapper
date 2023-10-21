2f34846a0fcb72c36109a19730798c156d70f1d2
dxmxtrxs <126823091+dxmxtrxs@users.noreply.github.com>
2023-10-21 17:21:31 +0300

    Update server.py

--- server.py
+++ server.py
@@ -13,19 +13,20 @@ def handle_client(client, users):
             message = client.recv(1024).decode("UTF-8")
             if message:
                 print(f"{users[client]}: {message}")
-
-                # Broadcasting the message to the connected clients
+                # Broadcast the message to all other connected clients
                 for other_client in users.keys():
                     if other_client != client:
                         send_message(other_client, users[client], message)
         except ConnectionResetError:
-            # In the case of a disconnection a message is sent notygi=ying the other user and the user that exited is deleted from the user dictionary
+            # Handle disconnection
             print(f"{users[client]} has disconnected.")
             del users[client]
+            for other_client in users.keys():
+                    send_message(other_client, sender="server", message=f"{users[client]} has disconnected.")
             break
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(("localhost", 9990))
+server.bind(("localhost", 9996))
 server.listen()
 
 users = {}  # Dictionary to store clients and their usernames
@@ -42,8 +43,11 @@ try:
         client_thread = threading.Thread(target=handle_client, args=(client, users))
         client_thread.start()
 
-except KeyboardInterrupt:
+except KeyboardInterrupt or ConnectionResetError:
     # If the server is quit by the user, close the server socket
     server.close()
     print("Server is shutting down.")
     sys.exit()
+    
+    
+    
